52be83f3a1c45fd7ff0cbe6de826b9001e29a75a
Jeison Pereira de Oliveira <jeison.po09@gmail.com>
2023-07-11 19:56:20 -0400

    Portado o código do my-app para o exercício de fixação

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Table from "./Table";
+class App extends Component {
+  render() {
+    return (
+      <div className="container text-center">
+        <Table />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import App from "./App";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
