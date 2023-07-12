dd8c389411d5fbb47b2706f623cc8c5b7e25bfd2
Jeison Pereira de Oliveira <jeison.po09@gmail.com>
2023-07-11 20:20:01 -0400

    Componente Navbar criado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import Table from "./Table";
+import { Component } from "react";
+import Navbar from "./components/Navbar";
 class App extends Component {
   render() {
     return (
       <div className="container text-center">
-        <Table />
+        <Navbar />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import App from "./App";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
